4ef6339dfcc0192ebbfbd71e237d89107d3fcfdf
Enzo Dragoun Arenhart <enzo.dragoun@rede.ulbra.br>
2023-08-25 21:49:07 -0300

    ex aula 04

--- exerciciosaula3.py
+++ exerciciosaula3.py
@@ -16,10 +16,29 @@ def int_list(list):
 
 def random_list(length):
     list = []
-    for item in range(length):
+    for _ in range(length):
         list.append(random.randint(1,1000))
     return list
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
     
+def count_numbers(n):
+    n_string = str(n)
+    number_list = []
+    other_list = []    
+    i = 0
+    
+    for number in n_string:
+        number_list.append(int(number))
+        
+    while i < 10:
+        other_list.append(number_list.count(i))
+        i +=1
+    return other_list
 
 while True:
     print("~~~~~~ MENU ~~~~~~")
@@ -29,6 +48,7 @@ while True:
     2 - Print a random list
     3 - Print list
     4 - Sort list
+    5 - Factor a number
     10 - Clear list
     0 - Exit
     """)
@@ -47,6 +67,11 @@ while True:
     elif option == 4:
         listaNum.sort()
         print(listaNum)
+    elif option == 5:
+        print("Inform a number to factor: ")
+        n = int(input())
+        print(factorial(n))
+        print(count_numbers(factorial(n)))
     elif option == 10:
         listaNum.clear()
     elif option == 0:
---
+++ exerciciosaula4.py
@@ -0,0 +1,62 @@
+##  Um algoritmo que utilize  as funcionalidades de uma lista, desempenhe o papel
+##de uma pilha, deve ter um menu que faça as seguintes operações
+
+main_list = []
+
+def empilhar(lista, ent):
+    for num in ent:
+        number = int(num)
+        lista.append(number)
+
+def desempilhar(lista):
+    if not lista:
+        print("Lista vazia!")
+    else:
+        lista.pop()
+    
+def limpar(lista):
+    if not lista:
+        print("Lista vazia!")
+    else:
+        lista.clear()
+        
+def print_pilha(lista):
+    if not lista:
+        print("Lista vazia!")
+    else:
+        for num in reversed(pilha):
+            print(num)
+
+    
+    
+def exibir_topo():
+    return main_list[-1]
+
+while True:
+    print(""" /\/\/\/\/\/ MENU \/\/\/\/\/\ \n
+1 - EMPILHAR
+2 - DESEMPILHAR
+3 - LIMPAR
+4 - EXIBIR TOPO
+5 - EXIBIR PILHA
+0 - SAIR
+    """)
+    print("Escolha uma opção")
+    op = int(input())
+   
+    if op == 0:
+       break
+    elif op == 1:
+        print("Digite os números a empilhar: ")
+        ent = input().split()
+        empilhar(main_list, ent)
+    elif op == 2:
+        print("Digite quantos números a serem desempilhados: ")
+        qtd = int(input())
+        main_list = desempilhar(main_list)
+    elif op == 3:
+        main_list = limpar(main_list)
+    elif op == 4:
+        print(f"Último número da pilha: {exibir_topo()}")
+    elif op == 5:
+        print_pilha(main_list)
