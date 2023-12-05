f694b10e4968a18fd8eee9c2b1e3ddca4df3ea7f
Enzo Dragoun Arenhart <enzo.dragoun@rede.ulbra.br>
2023-12-05 00:48:23 -0300

    AS - Pratica parte 2

--- AS/AS_bd/main.py
+++ AS/AS_bd/main.py
@@ -43,6 +43,10 @@ class Contato:
     def add_contato(self, nome, perfil_linkedin):
         self.nome = nome
         self.perfil_linkedin = perfil_linkedin
+        
+    def listar_ids(self):
+        return [contatos[0] for contatos in self.listar_contatos()]
+        
     
     def adicionar_contato(self):
         conn = mysql.connector.connect(**db_config)
@@ -70,12 +74,14 @@ class Contato:
         cursor.execute('BEGIN')
         
         try:
-            cursor.execute('DELETE FROM conexoes WHERE contato1_id = %i OR contato2_id = %i',(contato_id, contato_id))
-            cursor.execute('DELETE FROM contatos WHERE (id = %i)',(contato_id))
+            cursor.execute('DELETE FROM conexoes WHERE contato1_id = %s OR contato2_id = %s',(contato_id, contato_id))
+            cursor.execute('DELETE FROM contatos WHERE (id = %s)',(contato_id,))
             conn.commit()
-        except mysql.connector.errors as Error:
+        except mysql.connector.Error as Error:
             conn.rollback()
             print(f"Ocorreu um erro! {Error}")
+        
+        print("Contato excluido com sucesso!")
             
         conn.close()
         
@@ -89,106 +95,111 @@ class Contato:
         conn.close()
         return contatos
     
-    def listar_contato(self, contato_id):
+    def exibir_contato(self, contato_id):
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
         
         try:
-            cursor.execute('SELECT * FROM contatos WHERE (id = %i),', (contato_id))
+            cursor.execute('SELECT * FROM contatos WHERE (id = %s)', (contato_id,))
             contato = cursor.fetchone()
-        except mysql.connector.errors as Error:
+            return contato
+        except mysql.connector.Error as Error:
             conn.rollback()
             print(f"Ocorreu um erro! {Error}")
-            
+        
+        conn.close()
         return contato
-
-def adicionar_conexao(contato1_id, contato2_id):
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-    
-    cursor.execute('BEGIN')
-    
-    try:
-        cursor.execute('SELECT * FROM conexoes WHERE (contato1_id = %i AND contato2_id = %i) OR (contato1_id = %i AND contato2_id = %i)',
-                       (contato1_id, contato2_id, contato2_id, contato1_id))
-        if cursor.fetchone() is None:
-            cursor.execute('INSERT INTO conexoes (contato1_id, contato2_id) VALUES (%i, %i)', (contato1_id, contato2_id))
-        else:
-            print("A conexão entre esses contatos já existe!")
+            
         
-        conn.commit()
-    except mysql.connector.errors as Error:
-        conn.rollback()
-        print(f"Ocorreu um erro!\n {Error}")
-        
-    conn.close()
     
-def listar_conexoes(contato_id):
-    conn = mysql.connector.connect(**db_config)
-    cursor = conn.cursor()
-    
-    cursor.execute('''
-        SELECT contatos.nome
-        FROM contatos
-        JOIN conexoes ON contatos.id = CASE
-            WHEN conexoes.contato1_id = %i THEN conexoes.contato2_id
-            ELSE conexoes.contato1_id
-        END
-        WHERE conexoes.contato1_id = %i OR conexoes.contato2_id = %i               
-    ''', (contato_id, contato_id, contato_id))
-    
-    conexoes = cursor.fetchall()
-    conn.close()
-    return conexoes
-
 def menu():
     while True:
         contato = Contato()
+        
+        contatos = contato.listar_contatos()
+        lista_ids = contato.listar_ids()
+        
         print("""
         
         ~~~~~~~~ MENU ~~~~~~~~
         
         1 - ADICIONAR CONTATO
         2 - LISTAR CONTATOS
-        3 - ADICIONAR CONEXÃO
-        4 - LISTAR CONEXÕES DE UM CONTATO
-        5 - EXCLUIR CONTATO
+        3 - EXCLUIR CONTATO
+        4 - EXIBIR DETALHES DE UM CONTATO
         0 - SAIR
               
         """)
-
-        op = int(input("Escolha uma opção: "))
         
-        if op == 1:
-            nome = (input("Nome do contato: "))
-            perfil_linkedin = (input("Perfil do LinkedIn do contato: "))
-            contato.add_contato(nome, perfil_linkedin)
-            if contato.adicionar_contato() == 1:
-                print("Contato adicionado com sucesso!")
-            elif contato.adicionar_contato() == 0:
-                print("Contato já cadastrado!")
-            else: print("Erro ao cadastrar contato!")
-        elif op == 2:
-            contatos = contato.listar_contatos()
-            print("~~~~~~~~ LISTA DE CONTATOS ~~~~~~~~")
-            for contato in contatos:
-                print(f"ID: {contato[0]}, Nome: {contato[1]}, Perfil LinkedIn {contato[2]}")
-        elif op == 3:
-            contato1_id = int(input("ID do primeiro contato: "))
-            contato2_id = int(input("ID do segundo contato: "))
-            adicionar_conexao(contato1_id, contato2_id)
-        elif op == 4:
-            contato_id = input("Digite o ID do contato: ")
-            conexoes = listar_conexoes(contato_id)
-            print("Conexões do contato: ")
-            for conexao in conexoes:
-                print(conexao[0])
-        elif op == 5:
-            contato_id = input("Digige o ID do contato a ser excluido: ")
-            contato.excluir_contato(contato, contato_id)
-        elif op == 0:
-            break
+        try:
+            op = int(input("Escolha uma opção: "))
+            
+                        
+                        
+            if op == 1:
+                nome = (input("Nome do contato: "))
+                perfil_linkedin = (input("Perfil do LinkedIn do contato: "))
+                contato.add_contato(nome, perfil_linkedin)
+                if contato.adicionar_contato() == 1:
+                    print("Contato adicionado com sucesso!")
+                elif contato.adicionar_contato() == 0:
+                    print("Contato já cadastrado!")
+                else: print("Erro ao cadastrar contato!")
+                
+            elif op == 2:
+                
+                print("~~~~~~~~ LISTA DE CONTATOS ~~~~~~~~")
+                for contato in contatos:
+                    print(f"ID: {contato[0]}, Nome: {contato[1]}")
+
+            elif op == 3:
+                
+                print("~~~~~~~~ LISTA DE CONTATOS ~~~~~~~~")
+                for contatos in contatos:
+                    print(f"ID: {contatos[0]}, Nome: {contatos[1]}")
+                    
+                    
+                while True:
+                    try:
+                        contato_id = int(input("\nDigite o ID do contato a ser excluido(0 para sair): "))
+                        if contato_id == 0:
+                            break
+                        elif contato_id not in lista_ids:
+                            print("Contato nao encontrado, tente novamente.")
+                        else: 
+                            contato.excluir_contato(contato_id)
+                    except ValueError:
+                        print("Valor invalido. Tente novamente!")
+            
+            elif op == 4:
+                
+                while True:
+                    try:
+                        id = int(input("Digite o ID do Usuario para exibir mais detalhes(0 para sair): "))
+                        if id == 0:
+                            break
+                        elif id not in lista_ids:
+                            print("Contato nao encontrado, tente novamente.")
+                        else: 
+                            ex_contato = contato.exibir_contato(id)
+                            print(f"""
+                                
+            ~~~~~~ Contato {ex_contato[0]} ~~~~~~
+            
+            Nome do Contato: {ex_contato[1]}
+            Perfil do LinkedIn: {ex_contato[2]}
+                                
+                                """)
+                    except ValueError:
+                        print("Valor invalido. Tente novamente!")
+            
+            elif op == 0:
+                break
+        except ValueError:
+                    print("Valor invalido. Tente novamente!")
             
 if __name__ == "__main__":
     criar_banco(db_config)
+
     menu()
+    
